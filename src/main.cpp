77c69eba1773a49abc3db3c5d3e4563b84818d30
Haha64142 <paul.w64142@gmail.com>
2026-05-01 02:05:40 -0500

    Add logic and click function

--- src/main.cpp
+++ src/main.cpp
@@ -23,18 +23,77 @@
  * remembered pairs: unordered_map - key: pair id, value: tile number
  * */
 
+#include <array>
 #include <iostream>
+#include <unordered_map>
+#include <windows.h>
 
-int cropWidth = 351;
-int cropHeight = 248;
+constexpr int screenWidth = 1920;
+constexpr int screenHeight = 1080;
 
-int cropPosX[12] = {216, 590, 964, 1337, 216, 590, 964, 1337, 216, 590, 964, 1337};
-int cropPosY[12] = {215, 215, 215, 215, 485, 485, 485, 485, 754, 754, 754, 754};
+constexpr int cropWidth = 351;
+constexpr int cropHeight = 248;
 
-int clickPosX[12] = {391, 760, 1134, 1512, 391, 760, 1134, 1512, 391, 760, 1134, 1512};
-int clickPosY[12] = {339, 339, 339, 339, 608, 608, 608, 608, 881, 881, 881, 881};
+constexpr std::array<int, 12> cropPosX = {216, 590,  964, 1337, 216, 590,
+                                      964, 1337, 216, 590,  964, 1337};
+constexpr std::array<int, 12> cropPosY = {215, 215, 215, 215, 485, 485, 485, 485, 754, 754, 754, 754};
+
+constexpr std::array<int, 12> origClickPosX = {391,  760,  1134, 1512, 391,  760,
+                                           1134, 1512, 391,  760,  1134, 1512};
+constexpr std::array<int, 12> origClickPosY = {339, 339, 339, 339, 608, 608,
+                                           608, 608, 881, 881, 881, 881};
+
+constexpr std::array<int, 12> normalizePos(const std::array<int, 12> pos, int dimensionLength) {
+    std::array<int, 12> newPos = {};
+    for (size_t i = 0; i < 12; ++i) {
+        newPos.at(i) = pos.at(i) * 65535 / (dimensionLength - 1);
+    }
+    return newPos;
+}
+
+constexpr std::array<int, 12> clickPosX = normalizePos(origClickPosX, screenWidth);
+constexpr std::array<int, 12> clickPosY = normalizePos(origClickPosY, screenHeight);
+
+INPUT mouse[2] = {};
+
+void setupMouse() {
+    mouse[0].type = INPUT_MOUSE;
+    mouse[0].mi.dwFlags = MOUSEEVENTF_LEFTDOWN | MOUSEEVENTF_MOVE | MOUSEEVENTF_ABSOLUTE;
+
+    mouse[1].type = INPUT_MOUSE;
+    mouse[1].mi.dwFlags = MOUSEEVENTF_LEFTUP;
+}
+
+void click(int tileNumber) {
+    std::cout << "Click coords: " << clickPosX[tileNumber] << ", " << clickPosY[tileNumber] << '\n';
+    mouse[0].mi.dx = clickPosX[tileNumber];
+    mouse[0].mi.dy = clickPosY[tileNumber];
+
+    SendInput(2, mouse, sizeof(INPUT));
+}
 
 int main() {
-    std::cout << "Hello World\n";
+    setupMouse();
+
+    std::unordered_map<int, int> rememberedPairs;
+    int pairID = 0;
+
+    // TODO: Take main screenshot
+    for (size_t i = 0; i < 12; ++i) {
+        // TODO: crop tile 'i' and process
+        // TODO: convert cropped section to pair id
+        pairID = i / 2;
+        // std::cout << "Pair id: " << pairID << '\n';
+        std::unordered_map<int, int>::const_iterator it = rememberedPairs.find(pairID);
+        if (it != rememberedPairs.end()) {
+            // std::cout << "Clicking " << i << " and " << it->second << '\n';
+            click(i);
+            click(it->second);
+            rememberedPairs.erase(pairID);
+        } else {
+            // std::cout << "Adding " << i << '\n';
+            rememberedPairs.emplace(pairID, i);
+        }
+    }
     return 0;
 }
