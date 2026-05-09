b4534035ad7ce5e9021517e803383dc781202698
Haha64142 <paul.w64142@gmail.com>
2026-05-09 01:33:48 -0500

    Update to WW2 quizlet
    Also added a timer class for easier debugging

--- src/main.cpp
+++ src/main.cpp
@@ -44,6 +44,20 @@
 
 #include <tesseract/baseapi.h>
 
+class Timer {
+  private:
+    std::chrono::time_point<std::chrono::high_resolution_clock> startTime;
+    std::chrono::time_point<std::chrono::high_resolution_clock> endTime;
+
+  public:
+    void start() { startTime = std::chrono::high_resolution_clock::now(); }
+    void stop() { endTime = std::chrono::high_resolution_clock::now(); }
+    void print(const std::string &suffix = "") {
+        std::chrono::duration<double, std::micro> elapsed = endTime - startTime;
+        std::cout << elapsed.count() << "us" << suffix;
+    }
+};
+
 constexpr int screenWidth = 1920;
 constexpr int screenHeight = 1080;
 
@@ -84,107 +98,183 @@ std::vector<uint8_t> screenshotBuffer(screenWidth * screenHeight * 4);
 std::vector<uint8_t> cropBuffer(cropWidth * cropHeight * 4);
 std::vector<uint8_t> grayBuffer(cropWidth * cropHeight * 1);
 
+// Need to update size by hand. It's the highest pairID + 1
+std::array<int, 82> tileIdByPair;
+
 INPUT mouse[2] = {};
 
 tesseract::TessBaseAPI tess;
 
-const std::unordered_map<std::string, int> textData = {
-    {"The Election of", 0},
-    {"Republican Herbert Hoover", 0},
-    {"Stock Market", 1},
-    {"A system of", 1},
-    {"Asystem of", 1}, // Somtimes it doesn't see the space
-    {"Margin", 2},
-    {"Buying a stock", 2},
-    {"Margin Call", 3},
-    {"Demand by a", 3},
-    {"Installment Plan", 4},
-    {"People would make", 4},
-    {"Black Tuesday", 5},
-    {"On October prices", 5},
-    {"Banks in a", 6},
-    {"Depositors lost their", 6},
-    {"Overproduction/Surplus", 7},
-    {"Most economists agree", 7},
-    {"Dust Bowl", 8},
-    {"Poor agricultural practices", 8},
-    {"John Steinbeck", 9},
-    {"Wrote,The Grapes", 9},
-    {"Public Works", 10},
-    {"Projects such as", 10},
-    {"Reconstruction Finance Corporation", 11},
-    {"Set up to", 11},
-    {"Emergency Relief and", 12},
-    {"Called for billion", 12},
-    {"Farmers", 13},
-    {"In the summer", 13},
-    {"Hoovervilles", 14},
-    {"Because people blamed", 14},
-    {"Bonus Army", 15},
-    {"Veterans that set", 15},
-    {"Douglas MacArthur", 16},
-    {"Army chief of", 16},
-    {"Herbert Hoover", 17},
-    {"Failed to resolve", 17},
-    {"Franklin Roosevelt FDR", 18},
-    {"After serving as", 18},
-    {"New Deal", 19},
-    {"Roosevelt's policies", 19},
-    {"Bank Holiday", 20},
-    {"Closing of remaining", 20},
-    {"The Brain Trust", 21},
-    {"Name given to", 21},
-    {"The Emergency Banking", 22},
-    {"Required federal examiners", 22},
-    {"Fireside Chats", 23},
-    {"Direct talks FDR", 23},
-    {"Securities and Exchange", 24},
-    {"An independent agency", 24},
-    {"Federal Deposit Insurance", 25},
-    {"Provides government insurance", 25},
-    {"Agriculture Adjustment Administration", 26},
-    {"Paid farmers not", 26},
-    {"The Civilian Conservation", 27},
-    {"Employed single men", 27},
-    {"Federal Emergency Relief", 28},
-    {"Granted federal money", 28},
-    {"Public Works Administration", 29},
-    {"Provided employment in", 29},
-    {"The Civil Works", 30},
-    {"Worked somewhat like", 30},
-    {"American Liberty League", 31},
-    {"Business leaders and", 31},
-    {"Huey Long", 32},
-    {"The most serious", 32},
-    {"Works Progress Administration", 33},
-    {"Combated unemployment created", 33},
-    {"The Social Security", 34},
-    {"Its major goal", 34},
-    {"Tennessee Valley Authority", 35},
-    {"Program that brought", 35},
-    {"Supreme Court Court", 36},
-    {"Roosevelt wanted to", 36},
-    {"Hobos", 37},
-    {"Homeless wanderers who", 37},
-    // Deficit Spending has 2 different definitions
-    {"Deficit Spending", 39},
-    {"The economic theory", 39},
-    {"Spending more than", 39},
+Timer timer;
 
-    {"Okies", 40},
-    {"Farmers who packed", 40},
-    {"Loess", 41},
-    {"Windblown topsoil;lands", 41},
-    {"National Youth Administration", 42},
-    {"Provided jobs and", 42},
-    {"Federal Housing Administration", 43},
-    {"Very small down", 43},
-    {"Home Owners Loan", 44},
-    {"Refinance loans so", 44},
-    {"Soup Kitchens", 45},
-    {"During the Depression", 45},
-    {"Socialism", 46},
-    {"As the American", 46},
+const std::unordered_map<std::string, int> textData = {
+    {"Totalitarianism", 0},
+    {"is any political system in which a citizen is", 0},
+    {"Benito Mussolini", 1},
+    {"One of Europe's first major dictators.In after", 1},
+    {"One of Europe's first major dictators.In.after", 1}, // Sometimes messes up
+    {"Fascism", 2},
+    {"Refers to authoritarian political movement which ruled Italy under", 2},
+    {"Vladimir Lenin", 3},
+    {"Leader of the Bolshevik Party in Russia.Established the", 3},
+    {"Joseph Stalin", 4},
+    {"The Soviet dictator led the USSR through World War", 4},
+    {"Adolf Hitler", 5},
+    {"Leader of Nazi Germany during World War II", 5},
+    {"Mein Kampf My Struggle", 6},
+    {"Hitler's autobiography,in which Hitler calls for the", 6},
+    {"Manchuria", 7},
+    {"Resource-rich province in northern China.Japanese military leaders", 7},
+    {"The Nye Committee", 8},
+    {"Was responsible for documenting the huge profits that arms", 8},
+    {"Neutrality Act of", 9},
+    {"The act made it illegal for Americans to sell", 9},
+    {"Francisco Franco", 10},
+    {"led a rebellion in Spain,after the coalition of", 10},
+    {"Emperor Hirohito", 11},
+    {"The ruler of Japan during World War II", 11},
+    {"General Tojo", 12},
+    {"The military ruler of Japan", 12},
+    {"Axis-Powers", 13},
+    {"Together Germany,Italy,and Japan became known as the", 13},
+    {"Sudetenland", 14},
+    {"An area of Czechoslovakia with a large German-speaking", 14},
+    {"Neville Chamberlain", 15},
+    {"British prime minister who publicly promised to support France", 15},
+    {"The Munich Conference", 16},
+    {"On September Britain and France agreed to Hitler's", 16},
+    {"Poland", 17},
+    {"After the Munich conference,Hitler turned his sights on", 17},
+    {"The Nazi-Soviet Nonaggression Pact", 18},
+    {"Stalin agreed to the nonaggression pact with Germany because", 18},
+    {"Blitzkrieg", 19},
+    {"The Germans used a new type of warfare called", 19},
+    {"Rhineland", 20},
+    {"Area west of the Rhine River in which Hitler", 20},
+    {"Dunkirk", 21},
+    {"A small town in northern France near the Belgian", 21},
+    {"Vichy Regime", 22},
+    {"The puppet government set up by Hitler in Southern", 22},
+    {"Marshall Petain", 23},
+    {"The head of the Vichy Regime", 23},
+    {"Winston Churchill", 24},
+    {"Prime minister of Britain,who replaced Neville Chamberlain.Churchill", 24},
+    {"The Battle of Britain", 25},
+    {"The air battle between Britain and Germany that began", 25},
+    {"Royal Air Force R.A.F", 26},
+    {"Britain's Air Force", 26},
+    {"Hermann Goering", 27},
+    {"Head of the German air force,called the Luftwaffe", 27},
+    {"Luftwaffe", 28},
+    {"Germany's Air Force", 28},
+    {"The Nuremberg Laws", 29},
+    {"Laws set up by the Nazis that took rights", 29},
+    {"Gestapo", 30},
+    {"The Nazi governments secret police", 30},
+    {"Kristallnacht night of broken glass", 31},
+    {"The anti-Jewish violence that erupted throughout Germany and", 31},
+    {"Albert Einstein", 32},
+    {"In the early s Albert Einstein was among the", 32},
+    {"Holocaust", 33},
+    {"Hitler's attempt to destroy the Jews in Europe", 33},
+    {"Concentration Camps", 34},
+    {"Detention centers where Jews and other undesirables\"were sent", 34},
+    {"Detention centers where Jews and other undesirables'were sent", 34}, // Sometimes messes up
+    {"Extermination Camps", 35},
+    {"These were added to many concentration camps after the", 35},
+    {"The Neutrality Act of", 36},
+    {"Warring nations could buy weapons from the United States", 36},
+    {"The Lend-Lease Act", 37},
+    {"The United States would be able to lend or", 37},
+    {"The Atlantic Charter", 38},
+    {"It committed the two leaders to a postwar world", 38},
+    {"Pearl Harbor", 39},
+    {"The place where the Japanese tried to destroy the", 39},
+    {"December", 40},
+    {"The date that the Japanese bombed Pearl Harbor", 40},
+    {"Admiral Yamamoto", 41},
+    {"Japanese admiral that planned the sneak attack on Pearl", 41},
+    {"", 42},
+    {"Years of United States involvement in World War II", 42},
+    {"Franklin D.Roosevelt FDR", 43},
+    {"President of the United States through most of World", 43},
+    {"Selective Service and Training Act", 44},
+    {"First peacetime draft in the United States", 44},
+    {"General George Marshall", 45},
+    {"The chairman of the Joint Chiefs of Staff fought", 45},
+    {"General Dwight Eisenhower", 46},
+    {"The supreme allied commander in Europe", 46},
+    {"Operation Torch", 47},
+    {"The code name for the American invasion of North", 47},
+    {"Erwin Rommel", 48},
+    {"Germany's best field commander.Known as The Desert", 48},
+    {"General Bernard Montgomery", 49},
+    {"Great Britain's best field general", 49},
+    {"General George Patton", 50},
+    {"America's best field general.Known as Old Blood", 50},
+    {"General Mark Clark", 51},
+    {"General in charge of the Italian Campaign during World", 51},
+    {"Operation Barbarossa", 52},
+    {"The code name for the German invasion of the", 52},
+    {"Stalingrad", 53},
+    {"The German defeat at this city was the turning", 53},
+    {"D-Day/Operation Overlord", 54},
+    {"Codename for the Allied invasion of France", 54},
+    {"June", 55},
+    {"The date of the Allied invasion of France", 55},
+    {"Battle of the Bulge", 56},
+    {"The largest battle fought by the United States Forces", 56},
+    {"Audie Murphy", 57},
+    {"Served in the European Theatre,is the most decorated", 57},
+    {"V-E Day", 58},
+    {"Victory in Europe,May", 58},
+    {"General Douglas MacArthur", 59},
+    {"The supreme Allied commander in the Pacific", 59},
+    {"Chester Nimitz", 60},
+    {"Top Admiral in the United States Navy during World", 60},
+    {"Coral Sea", 61},
+    {"First naval battle ever fought using only airplanes;this", 61},
+    {"Guadalcanal", 62},
+    {"First time the American forces land on an island", 62},
+    {"Island Hopping", 63},
+    {"American strategy used to defeat Japan;Americans would only", 63},
+    {"Midway", 64},
+    {"The United States victory here was the turning point", 64},
+    {"Leyte Gulf", 65},
+    {"The victory in which the United States navy destroyed", 65},
+    {"Kamikaze", 66},
+    {"Japanese suicide planes,one plane one ship", 66},
+    {"Asian Americans", 67},
+    {"During World War II,the United States government placed", 67},
+    {"Korematsu v.United States", 68},
+    {"Supreme Court case that ruled relocation of Japanese Americans", 68},
+    {"Bataan Death March", 69},
+    {"Took place in the Philippines", 69},
+    {"Iwo Jima/Okinawa", 70},
+    {"lwo Jima/Okinawa", 70}, // Sometimes messes up 'Iwo'
+    {"Because of the heavy casualties suffered on these islands", 70},
+    {"Ernie Pyle", 71},
+    {"Famous reporter for Stars and Stripes;he was killed", 71},
+    {"Manhattan Project", 72},
+    {"Code name for the development of the atomic bomb", 72},
+    {"Dr.Oppenheimer", 73},
+    {"The man in charge of the Manhattan Project", 73},
+    {"Harry S Truman", 74},
+    {"The American president that made the decision to drop", 74},
+    {"USS Indianapolis", 75},
+    {"Ship that delivered the atomic bomb to Tinian Island", 75},
+    {"Enola Gay", 76},
+    {"The B2bomber that dropped the first atomic bomb", 76},
+    {"Hiroshima", 77},
+    {"The city selected as the target for the first", 77},
+    {"The Bock's Car", 78},
+    {"The B2bomber that dropped the second atomic bomb", 78},
+    {"Nagasaki", 79},
+    {"The city selected as the target for the second", 79},
+    {"USS Missouri", 80},
+    {"Ship on which the Japanese surrendered at the end", 80},
+    {"V-J Day", 81},
+    {"Victory in Japan,September", 81},
 };
 
 inline void sleep_ms(int millis) { std::this_thread::sleep_for(std::chrono::milliseconds(millis)); }
@@ -205,7 +295,7 @@ int initTesseract() {
     }
     tess.SetPageSegMode(tesseract::PageSegMode::PSM_SINGLE_BLOCK);
     tess.SetVariable("tessedit_char_whitelist",
-                     "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz,;'/ ");
+                     "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz,;'/ -.0123456789\"");
     return 0;
 }
 
@@ -384,16 +474,17 @@ std::string getFirstWords(const std::string &inText, int idealWords = 3) {
 }
 
 void ocrThread() {
-    auto start = std::chrono::steady_clock::now();
-
-    std::unordered_map<int, int> rememberedPairs;
     int pairID = 0;
     std::string text;
 
+    timer.start();
     const std::vector<uint8_t> fullScreenshot = takeScreenshot();
+    timer.stop();
+    timer.print("\n");
 
     for (size_t i = 0; i < 12; ++i) {
-        text = getFirstWords(getText(grayscale(crop(fullScreenshot, i)), cropWidth, cropHeight));
+        timer.start();
+        text = getFirstWords(getText(grayscale(crop(fullScreenshot, i)), cropWidth, cropHeight), 9);
 
         auto itText = textData.find(text);
         if (itText != textData.end()) {
@@ -402,20 +493,19 @@ void ocrThread() {
             std::cout << "Text for tile " << i << " is: " << text << "\n";
         }
 
-        auto it = rememberedPairs.find(pairID);
-        if (it != rememberedPairs.end()) {
+        if (tileIdByPair[pairID] == -1) {
+            tileIdByPair[pairID] = i;
+        } else {
             {
                 std::lock_guard<std::mutex> lock(queueMutex);
 
-                clickQueue.push(it->second);
+                clickQueue.push(tileIdByPair[pairID]);
                 clickQueue.push(i);
             }
             cv.notify_one();
-
-            rememberedPairs.erase(pairID);
-        } else {
-            rememberedPairs.emplace(pairID, i);
         }
+        timer.stop();
+        timer.print("\n");
     }
 
     {
@@ -424,14 +514,13 @@ void ocrThread() {
     }
     cv.notify_one();
 
-    auto end = std::chrono::steady_clock::now();
-    std::chrono::duration<double, std::milli> elapsed = end - start;
-    std::cout << "\nDuration: " << elapsed.count() << "ms\n";
+    tileIdByPair.fill(-1);
 }
 
 void clickThread() {
     int tile;
     std::cout << "Clicking: \n";
+    // bool timed = false;
 
     while (true) {
         {
@@ -446,13 +535,17 @@ void clickThread() {
             clickQueue.pop();
         }
 
+        // if (!timed) {
+        //     timed = true;
+        // }
         click(tile);
-        std::cout << tile << ", ";
-        sleep_ms(160);
+        // std::cout << tile << ", ";
+        sleep_ms(155);
     }
 
-    std::cout << "\nDone\n\n";
     finished = false;
+    // timed = true;
+    std::cout << "\nDone\n\n";
 }
 
 int main() {
@@ -462,6 +555,7 @@ int main() {
         return initResult;
     if (setupHotkeys())
         return 1;
+    tileIdByPair.fill(-1);
 
     std::cout << "Press ALT + R to start the quizlet matching\n"
                  "Press CTRL + ALT + C to exit\n\n";
